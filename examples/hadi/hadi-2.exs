5380404d0053e34f500834e926d850d948631da4
Craig S. Cottingham <craig@cottingham.net>
2020-01-05 16:57:51 -0600

    Add event listeners for the legs

--- examples/hadi/hadi-2.exs
+++ examples/hadi/hadi-2.exs
@@ -11,7 +11,9 @@ defmodule Hadi_2 do
     CelestialBody,
     Control,
     Flight,
+    Leg,
     Orbit,
+    Parts,
     ReferenceFrame,
     Vessel
   }
@@ -25,6 +27,7 @@ defmodule Hadi_2 do
       |> initialize_ins()
       |> initialize_vessel()
       |> initialize_controllers()
+      |> initialize_event_listeners()
 
     Control.activate_next_stage(state.control)
     Control.set_throttle(state.control, 1.0)
@@ -32,9 +35,38 @@ defmodule Hadi_2 do
     ascend_to(10.0, Flight.surface_altitude(state.flight), state)
     coast_to_max(Flight.vertical_speed(state.flight), state)
     hover(state)
-    descend_until(Vessel.situation(state.vessel), state)
+    descend_until_landed(state)
 
     Control.set_throttle(state.control, 0.0)
+    Enum.each(state.leg_listeners, fn listener -> Process.exit(listener, :normal) end)
+  end
+
+  ## these functions need to be public or we get a warning about them being unused
+  def leg_is_grounded(leg, grounded_state, parent)
+
+  def leg_is_grounded(leg, nil, parent) do
+    send(parent, {:is_grounded, Leg.is_grounded(leg), leg})
+    leg_is_grounded(leg, Leg.is_grounded(leg), parent)
+  end
+
+  def leg_is_grounded(leg, true, parent) do
+    if (Leg.is_grounded(leg)) do
+      Process.sleep(1)
+      leg_is_grounded(leg, true, parent)
+    else
+      send(parent, {:is_grounded, false, leg})
+      leg_is_grounded(leg, false, parent)
+    end
+  end
+
+  def leg_is_grounded(leg, false, parent) do
+    if (Leg.is_grounded(leg)) do
+      send(parent, {:is_grounded, true, leg})
+      leg_is_grounded(leg, true, parent)
+    else
+      Process.sleep(1)
+      leg_is_grounded(leg, false, parent)
+    end
   end
 
   defp ascend_to(target_altitude, altitude, %{control: control}) when (altitude >= target_altitude), do: Control.set_throttle(control, 0.0)
@@ -43,25 +75,34 @@ defmodule Hadi_2 do
   defp coast_to_max(vertical_speed, _state) when (vertical_speed < 0.0), do: Process.send_after(self(), :descend, 10_000)
   defp coast_to_max(_, %{flight: flight} = state), do: coast_to_max(Flight.vertical_speed(flight), state)
 
-  defp descend_until(:landed, _), do: :ok
+  defp descend_until_landed(%{control: control, flight: flight, descent_controller: controller} = state) do
+    receive do
+      {:is_grounded, true, _} ->
+        :ok
 
-  defp descend_until(_, %{vessel: vessel, control: control, flight: flight, descent_controller: controller} = state) do
-    Control.set_pitch(control, 0.0)
-    Control.set_roll(control, 0.0)
+    after
+      0 ->
+        Control.set_pitch(control, 0.0)
+        Control.set_roll(control, 0.0)
 
-    vertical_speed = Flight.vertical_speed(flight)
-    {:ok, throttle_value, controller} = PidController.output(vertical_speed, controller)
-    Control.set_throttle(control, throttle_value)
-    descend_until(Vessel.situation(vessel), %{state | descent_controller: controller})
+        vertical_speed = Flight.vertical_speed(flight)
+        {:ok, throttle_value, controller} = PidController.output(vertical_speed, controller)
+        Control.set_throttle(control, throttle_value)
+        descend_until_landed(%{state | descent_controller: controller})
+    end
   end
 
   defp hover(%{control: control, flight: flight, hover_controller: controller} = state) do
     receive do
+      {:is_grounded, _, _} ->
+        # flush out any events related to change of grounded state of the legs
+        hover(state)
+
       :descend ->
         :ok
 
     after
-      10 ->
+      0 ->
         Control.set_pitch(control, 0.0)
         Control.set_roll(control, 0.0)
 
@@ -84,6 +125,18 @@ defmodule Hadi_2 do
     state
     |> Map.put(:hover_controller, hover_controller)
     |> Map.put(:descent_controller, descent_controller)
+  end
+
+  defp initialize_event_listeners(state) do
+    parent = self()
+
+    listeners =
+      state.vessel
+      |> Vessel.parts()
+      |> Parts.legs()
+      |> Enum.map(fn leg -> spawn(__MODULE__, :leg_is_grounded, [leg, nil, parent]) end)
+
+    Map.put(state, :leg_listeners, listeners)
   end
 
   defp initialize_ins(%{conn: conn, flight: flight, vessel: vessel} = state) do
